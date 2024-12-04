97187c6f4a78a17ff7d6a64c22290ccc6ada2fcc
Joel Danielz <joeldanielz@icloud.com>
2024-12-04 13:40:14 +0000

    New Employee form finished

--- API/aiimi.employee.directory.api.BusinessLayer/Services/EmployeeDataService.cs
+++ API/aiimi.employee.directory.api.BusinessLayer/Services/EmployeeDataService.cs
@@ -62,7 +62,7 @@ namespace aiimi.employee.directory.api.BusinessLayer.Services
         /// <param name="employee">The <see cref="EmployeeDto"/> object representing the new employee to add.</param>
         /// <returns>An asynchronous task representing the operation.</returns>
         /// <exception cref="IOException">Thrown if there is an issue writing to the file.</exception>
-        public Task AddEmployee(EmployeeDto employee)
+        public async Task<EmployeeDto> AddEmployee(EmployeeDto employee)
         {
             try
             {
@@ -78,7 +78,7 @@ namespace aiimi.employee.directory.api.BusinessLayer.Services
                 throw new IOException("Error writing to the employee data file.", ex);
             }
 
-            return Task.CompletedTask;
+            return await Task.FromResult(employee);
         }
     }
 }
--- Frontend/aiimi-employee-directory/src/components/NewEmployeeForm.tsx
+++ Frontend/aiimi-employee-directory/src/components/NewEmployeeForm.tsx
@@ -1,6 +1,11 @@
 import { useState } from "react";
+import { addEmployee } from "./employee-list/employeesSlice";
+import { useDispatch } from "react-redux";
+import { AppDispatch } from "../store";
+import '../styles/NewEmployeeForm.scss'
 
 const NewEmployeeForm = () => {
+  const dispatch = useDispatch<AppDispatch>();
   const [newEmployee, setNewEmployee] = useState({
     firstName: "",
     lastName: "",
@@ -10,7 +15,65 @@ const NewEmployeeForm = () => {
   });
   // dispatch(addEmployee({firstName: "joel", lastName: "danielz", jobTitle: "dev", phone: "07983808138", email: "joedanielz@hotmail.com"})); // Dispatch the `add` action
 
-  return <div>Hello!</div>;
+  const handleCreateEmployee = () => {
+    dispatch(addEmployee(newEmployee));
+  };
+
+  return (
+    <div className="Employee-Form-Container">
+      <input
+        type="text"
+        placeholder="First Name"
+        style={{width: "40%"}}
+        className="Employee-Form-Text-Input"
+        value={newEmployee.firstName}
+        onChange={(e) =>
+          setNewEmployee({ ...newEmployee, firstName: e.target.value })
+        }
+      />
+      <input
+        type="text"
+        placeholder="Last Name"
+        style={{width: "40%"}}
+        className="Employee-Form-Text-Input"
+        value={newEmployee.lastName}
+        onChange={(e) =>
+          setNewEmployee({ ...newEmployee, lastName: e.target.value })
+        }
+      />
+      <input
+        type="text"
+        placeholder="Job Title"
+        style={{width: "30%"}}
+        className="Employee-Form-Text-Input"
+        value={newEmployee.jobTitle}
+        onChange={(e) =>
+          setNewEmployee({ ...newEmployee, jobTitle: e.target.value })
+        }
+      />
+      <input
+        type="text"
+        placeholder="Phone"
+        style={{width: "30%"}}
+        className="Employee-Form-Text-Input"
+        value={newEmployee.phone}
+        onChange={(e) =>
+          setNewEmployee({ ...newEmployee, phone: e.target.value })
+        }
+      />
+      <input
+        type="email"
+        placeholder="Email"
+        style={{width: "30%"}}
+        className="Employee-Form-Text-Input"
+        value={newEmployee.email}
+        onChange={(e) =>
+          setNewEmployee({ ...newEmployee, email: e.target.value })
+        }
+      />
+      <button className="Employee-Form-Button" onClick={handleCreateEmployee}>Create</button>
+    </div>
+  );
 };
 
 export default NewEmployeeForm;
